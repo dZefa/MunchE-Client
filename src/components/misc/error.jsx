87bfbf7df5c62304c7a0bfa505c742dd0c85494b
Daniel Chong <danchong625@gmail.com>
2017-12-06 18:28:16 -0800

    [feat] initializes default pages

---
+++ src/components/misc/error.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Error404 extends Component {
+  constructor() {
+    super();
+  }
+
+  render() {
+    return (
+      <div>YOU IN THE WRONG PLACE</div>
+    );
+  }
+};
+
+export default Error404;
---
+++ src/components/misc/loading.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Loading extends Component {
+  constructor() {
+    super();
+  }
+  
+  render() {
+    return (
+      <div>LOADING CIRCLE THINGY GOES HERE</div>
+    );
+  }
+};
+
+export default Loading;
